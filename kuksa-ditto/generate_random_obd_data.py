4d7e999427c7ed44a99c3586a972b5cf08f20214
Zubair <91695367+zubxxr@users.noreply.github.com>
2025-03-29 16:14:49 -0400

    Create generate_random_obd_data.py

---
+++ kuksa-ditto/generate_random_obd_data.py
@@ -0,0 +1,22 @@
+def main():
+    # Repeat Infinitely
+    while True:
+        # Generate random values for each feature with the defined ranges
+        VehicleSpeed = ___________
+        EngineSpeed = ___________
+        ThrottlePosition = ___________
+        CoolantTemperature = ___________
+
+        # Print the value for each feature
+        print('Vehicle Speed =', ___________)
+        print('Engine Speed =', ___________)
+        print('Throttle Position =', ___________)
+        print('Coolant Temperature =', ___________)
+
+        # Pause for 1 second
+        ____.sleep(1)
+
+        print('-----------------------------')
+
+# Run the main function
+main()
